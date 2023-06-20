3e69e7802bcc13d514f1ad2a868fe180db57abe2
khomabhena <colwanymab@gmail.com>
2023-06-20 21:56:18 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require("./routes/index")
+const usersRouter = require("./routes/users")
+const catalogRouter = require("./routes/catalog")
+
+const compression = require("compression");
+const helmet = require('helmet')
+
+const app = express();
+const mongoose = require('mongoose')
+mongoose.set("strictQuery", false)
+const mongoDB = "mongodb+srv://khomabhena:ColwanyMab28@cluster0.krkkba9.mongodb.net/local_library?retryWrites=true&w=majority"
+
+main().then(console.log('Connected to Database')).catch((err) => console.log(err))
+async function main() {
+  await mongoose.connect(mongoDB)
+}
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+app.use(compression())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ models/author.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const { DateTime } = require('luxon')
+
+const Schema = mongoose.Schema
+
+const AuthorSchema = new Schema({
+    first_name: {
+        type: String, 
+        required: true, 
+        maxLength: 100
+    },
+    family_name: {
+        type: String, 
+        required: true, 
+        maxLength: 100
+    }, 
+    date_of_birth: {
+        type: Date 
+    },
+    date_of_death: { 
+        type: Date 
+    }
+})
+
+AuthorSchema.virtual("name").get(function () {
+    let fullname = ""
+    if (this.first_name && this.family_name) {
+        fullname = `${this.first_name}, ${this.family_name}`
+    }
+
+    return fullname
+})
+
+AuthorSchema.virtual("url").get(function () {
+    return `/catalog/author/${this._id}`
+})
+
+AuthorSchema.virtual("birth_date").get(function () {
+    return this.date_of_birth ? 
+        DateTime.fromJSDate(this.date_of_birth)
+        .toLocaleString(DateTime.DATE_MED) : ''
+})
+
+AuthorSchema.virtual("death_date").get(function () {
+    return this.date_of_death ? 
+        DateTime.fromJSDate(this.date_of_death)
+        .toLocaleString(DateTime.DATE_MED) : ''
+})
+
+// Export model
+module.exports = mongoose.model("Author", AuthorSchema)
